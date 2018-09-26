3a1b21973c683a24481d430592b4a7ef4da39aa6
miscbits <willow6786@gmail.com>
2018-09-26 15:19:55 -0400

    added CORS middleware and added user is staff and user is student middleware

--- app/middleware/UserIsStaff.js
+++ app/middleware/UserIsStaff.js
@@ -1,3 +1,20 @@
 module.exports = (req, res, next) => {
+	var access_token = req.headers.access_token;
+
+    axios.get("https://www.googleapis.com/plusDomains/v1/people/me", {
+        headers: {
+            Authorization: "Bearer " + access_token
+        }
+    })
+    .then((person) => {
+        if (person.domain != "zipcodewilmington.com") {
+            return res.status(413).json({message: "Only Zipcode Wilmington Staff may use this API"});
+        }
+    })
+    .catch((err) => {
+        return next(err)
+    });
+
+
 	return next();
 }
