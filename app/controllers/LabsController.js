cd33362d03220f9da34d569d895b3089f109bf5e
miscbits <willow6786@gmail.com>
2018-09-27 15:37:21 -0400

    created lab crud

--- app/controllers/LabsController.js
+++ app/controllers/LabsController.js
@@ -1,4 +1,6 @@
-const Labs = require("../models/labs")
+const sequelize = require('../models').sequelize;
+const Labs = sequelize.import("../models/labs");
+
 
 module.exports = {
   index: index,
@@ -9,37 +11,79 @@ module.exports = {
 };
 
 function index(req, res, next) {
-
-	return res.status(200)
-        .json({
-          status: 'success'
-        });
+    Lab.findAll()
+    .then((labs => {
+      return res.status(200)
+        .json(labs);    
+    })
+  );
 }
 
 function show(req, res, next) {
-	return res.status(200)
-        .json({
-          status: 'success'
-        });
+  Lab.findById(req.params.id)
+    .then((labs => {
+      return res.status(200)
+        .json(labs);    
+    })
+  );
 }
 
 function store(req, res, next) {
-	return res.status(200)
-        .json({
-          status: 'success'
-        });
+  transientLab = {
+      url: req.body.url
+    , name: req.body.name
+    , due_date: req.body.due_date
+    , assigned_date: req.body.assigned_date
+  }
+
+  Lab.build(transientLab)
+    .save()
+    .then((lab) => {
+      return res.status(201)
+        .json(lab);    
+    })
+    .catch(err => {
+      return next(err);
+    }
+  );
+
 }
 
 function update(req, res, next) {
-	return res.status(200)
-        .json({
-          status: 'success'
-        });
+    Lab.findById(req.params.id)
+        .then((lab) => {
+            let updateVals = {};
+
+            if (req.body.url) {
+                updateVals.url = req.body.url;
+            }
+            if (req.body.name) {
+                updateVals.name = req.body.name;
+            }
+            if (req.body.due_date) {
+                updateVals.due_date = req.body.due_date;
+            }
+            if (req.body.assigned_date) {
+                updateVals.assigned_date = req.body.assigned_date;
+            }
+
+            lab.update(updateVals)
+                .then((updatedLab) => {
+                    return res.status(200).json(updatedLab);
+                })
+        })
+        .catch(err => {
+            return res.status(404).json({message: "Lab not found"});
+        }
+    );
 }
 
 function destroy(req, res, next) {
-	return res.status(200)
-        .json({
-          status: 'success'
-        });
+    const affected_rows = Lab.destroy({
+        where: {
+          id: req.params.id
+        }
+    });
+
+    return res.status(200).json({message: "success"});
 }
--- app/controllers/StudentController.js
+++ app/controllers/StudentController.js
@@ -92,6 +92,4 @@ function destroy(req, res, next) {
     });
 
     return res.status(200).json({message: "success"});
-
-
 }
