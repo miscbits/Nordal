f91701092f12f6f4e2bea63989b24846e6d74981
miscbits <willow6786@gmail.com>
2018-10-05 11:50:20 -0400

    removed env from database config file

--- app/config/database.js
+++ app/config/database.js
@@ -1,11 +1,9 @@
-const env = require('dotenv').load().parsed;
-
 module.exports = {
   development: {
-    username: env.DB_USER,
-    password: env.DB_PASS,
-    database: env.DB_DATABASE,
-    host: env.DB_HOST,
+    username: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_DATABASE,
+    host: process.env.DB_HOST,
     dialect: 'postgres',
     define: {
       createdAt: 'created_at',
