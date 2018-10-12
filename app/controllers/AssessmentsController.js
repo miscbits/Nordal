ffef6c4475e6368c65edca8655d721807fc176cc
miscbits <willow6786@gmail.com>
2018-10-12 15:59:58 -0400

    include submissions and students in assessment api call

--- app/controllers/AssessmentsController.js
+++ app/controllers/AssessmentsController.js
@@ -19,7 +19,17 @@ function index(req, res, next) {
 }
 
 function show(req, res, next) {
-  Assessment.findById(req.params.id)
+  Assessment.findById(req.params.id, {
+    include: [{
+        model: models.submissions,
+        as: "submissions",
+        required: false,
+        include: [{
+          model: models.students,
+          as: 'students'
+        }]
+      }]
+    })
     .then((assessments => {
       return res.status(200)
         .json(assessments);
