c71e7048cf69b2ec58a601d8e75c1f7aaee68cab
miscbits <willow6786@gmail.com>
2018-10-12 17:51:55 -0400

    need to check checksum and secret because either could exist and the code will still work

--- app/middleware/SecureGithubWebhook.js
+++ app/middleware/SecureGithubWebhook.js
@@ -11,8 +11,7 @@ module.exports = (req, res, next) => {
   const checksum = req.headers['x-hub-signature']
 
   //git tea doesnt support digest checks so you have to just check the secret in the payload  
-  if (!checksum || !digest || (checksum !== digest && process.env.GITHUB_WEBHOOK_SECRET !== req.body.secret)) {
-    console.log((checksum !== digest && process.env.GITHUB_WEBHOOK_SECRET !== req.body.secret));
+  if ((!checksum && !req.body.secret) || !digest || (checksum !== digest && process.env.GITHUB_WEBHOOK_SECRET !== req.body.secret)) {
     return res.status(401).json("Request body digest did not match X-Hub-Signature");
   }
   return next()
