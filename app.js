9eeb1a0be86f515aff5a517b80e6b8ef0a18058d
miscbits <willow6786@gmail.com>
2018-10-12 11:21:07 -0400

    catch options requests and send a 200

--- app.js
+++ app.js
@@ -28,7 +28,11 @@ app.use(cookieParser());
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "*");
-  next();
+  if ('OPTIONS' === req.method) {
+      res.send(200);
+  } else {
+    next();
+  }
 });
 
 app.use('/', indexRouter);
